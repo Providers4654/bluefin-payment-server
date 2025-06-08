20aa0cd9bb42119499c0af23a4d44c7d669acba5
Providers4654 <providers@mtnhlth.com>
2025-06-08 17:03:23 -0600

    Update index.ts

--- api/payment/index.ts
+++ api/payment/index.ts
@@ -4,10 +4,11 @@ export default async function handler(req, res) {
   const origin = req.headers.origin;
 
   // ✅ CORS headers
-  res.setHeader("Access-Control-Allow-Origin", origin || "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+ res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Allow-Methods", "*");
+res.setHeader("Access-Control-Allow-Headers", "*");
+
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
