67f20bbd4b27b7b04bbba5fe122cfa5c5fc28176
Providers4654 <providers@mtnhlth.com>
2025-06-08 10:58:05 -0600

    Update index.ts

--- api/payment/index.ts
+++ api/payment/index.ts
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://www.mtnhlth.com");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
     return res.status(200).end();
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://www.mtnhlth.com");
 
   const { token, amount, name } = req.body;
 
