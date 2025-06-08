89ee9625d4b74d708903b17c592ebca36045d27b
Providers4654 <providers@mtnhlth.com>
2025-06-08 17:19:20 -0600

    use *.mtnhlth.com wildcard for CORS

--- api/payment/index.ts
+++ api/payment/index.ts
@@ -2,10 +2,10 @@ export default async function handler(req, res) {
   const origin = req.headers.origin;
 
   // ✅ CORS headers
- res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Allow-Methods", "*");
-res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://*.mtnhlth.com");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
 
   if (req.method === "OPTIONS") {
